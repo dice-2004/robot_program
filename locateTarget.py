9da05e889491f36b6e2fa0a0ce19c916f8748cbc
FUJITAKONNRO <rakkukusu@gmail.com>
2025-11-14 14:28:46 +0900

    緑検知

--- locateTarget.py
+++ locateTarget.py
@@ -87,3 +87,28 @@ def locateTarget(imInputHSV):
 		sVertical = -1
 		sSize = -1
 	return (sHorizontal, sVertical, sSize), imBlueBinary
+
+def locateCylinder(imInputHSV):
+	# 対象色の定義（緑の場合）
+	vMinHSV = np.array([40,180,0])
+	vMaxHSV = np.array([46,255,255])
+	imGreen = cv2.inRange(imInputHSV, vMinHSV, vMaxHSV)
+	
+	# 対象色のエリア画像の作成
+	imGreenBinary = imGreen / 255
+
+	# 対象色エリア（最も縦に長いもの）の水平位置の割り出し
+	vSumGreenVertical = np.sum(imGreenBinary, axis=0)
+	sMaxIndex = vSumGreenVertical.argmax()
+	
+	# 対象色エリアの縦の長さが5画素よりも大きい場合、ターゲットに設定
+	if vSumGreenVertical[sMaxIndex] > 5:
+		sHorizontal = sMaxIndex
+		sVertical = -1
+		sSize = vSumGreenVertical[sMaxIndex]
+	else:
+		sHorizontal = -1
+		sVertical = -1
+		sSize = -1
+
+	return (sHorizontal, sVertical, sSize), imGreenBinary
