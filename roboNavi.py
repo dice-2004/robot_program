16b689abd62f320a8f21147bbb51cdf04f136291
FUJITAKONNRO <rakkukusu@gmail.com>
2025-11-21 14:02:12 +0900

    green_test

--- roboNavi.py
+++ roboNavi.py
@@ -118,9 +118,9 @@ while videoCap.isOpened() :
 
 	# 画像の表示 ----------------------------------------------
 	if sFrame % sDisplayRate == 0:
-		cv2.imshow('input', imDisplay)
+		#cv2.imshow('input', imDisplay)
 		#cv2.imshow('red', imRedBinary)
-		#cv2.imshow('green', imGreenBinary)
+		cv2.imshow('green', imGreenBinary)
 		#cv2.imshow('blue', imBlueBinary)
 		#cv2.imshow('yellow', imYellowBinary)
 
